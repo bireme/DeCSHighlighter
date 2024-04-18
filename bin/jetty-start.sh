682b0500372f95e54e575690eec1b858228f0a04
superheitor <superheitor@gmail.com>
2024-04-18 18:06:18 -0300

    version 3.6.4

--- bin/jetty-start.sh
+++ bin/jetty-start.sh
@@ -3,7 +3,9 @@
 DH_DIR=/home/javaapps/sbt-projects/DeCSHighlighter
 cd $DH_DIR/jetty-base || exit
 
-../jetty-home-12.0.8/bin/jetty.sh start
+../jetty-home-11.0.14/bin/jetty.sh start
+#../jetty-home-12.0.8/bin/jetty.sh start    # mudar DH_DIR
+
 ret="$?"
 
 sleep 120s
